68f6375ee4591b7771df8a96661dc6ad040c765d
nicacio <g.hiFW@sD4Jj9t$>
2020-04-21 17:08:35 -0300

    add base and index.html.

--- StockSys/settings.py
+++ StockSys/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'StockSys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'StockApp/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
